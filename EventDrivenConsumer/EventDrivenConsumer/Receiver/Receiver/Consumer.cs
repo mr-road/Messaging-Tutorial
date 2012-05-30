720e5a121f523205847cda686ceea9cd1391b18d
James Atherton <james.atherton@gmail.com>
2012-05-30 12:27:33 +0100

    Got the event driven system working

--- EventDrivenConsumer/EventDrivenConsumer/Receiver/Receiver/Consumer.cs
+++ EventDrivenConsumer/EventDrivenConsumer/Receiver/Receiver/Consumer.cs
@@ -1,48 +1,78 @@
 using System;
 using System.Messaging;
+using MessageUtilities;
 
 namespace Receiver
 {
     internal class Consumer 
     {
-        private readonly MessageQueue channel;
-        private bool isRunning;
+        private readonly MessageQueue _channel;
+        private bool _isRunning;
 
         public Consumer(string channelName)
         {
             //We need to identify how the message is formatted - xml is the default
-            channel = new MessageQueue(channelName) {Formatter = new XmlMessageFormatter(new[] {typeof (string)})};
+			
+
+            _channel = new MessageQueue(channelName) {Formatter = new XmlMessageFormatter(new[] {typeof (string)})};
             //We want to trace message headers such as correlation id, so we need to tell MSMQ to retrieve those
-            channel.MessageReadPropertyFilter.SetAll();
+            _channel.MessageReadPropertyFilter.SetAll();
             //TODO: Set up a callback for the recieve completed event that calls the Consume method
+        	_channel.ReceiveCompleted += Consume;
         }
 
         public void Start()
         {
-            isRunning = true;
-            //TODO: On start we need to wait on messages from the queue
+            _isRunning = true;
+        	Receive();
             Console.WriteLine("Service started");
         }
 
 
         public void Pause()
         {
-            isRunning = false;
+            _isRunning = false;
             Console.WriteLine("Service paused");
         }
 
         public void Stop()
         {
-            isRunning = false;
-            channel.Close();
+            _isRunning = false;
+            _channel.Close();
             Console.WriteLine("Service stopped");
         }
 
         private void Consume(object source, ReceiveCompletedEventArgs result)
         {
             //TODO: The Consume method is called on completion of a message being received
+
             //TODO: We need to obtain the message from the result by calling EndRecieve
             //TODO: We also want to begin recieving again, unless we have stopped running
+
+			try
+			{
+				var queue = (MessageQueue) source;
+				var message = queue.EndReceive(result.AsyncResult);
+				if (message != null)
+				{
+					message.TraceMessage();
+				}
+			}
+			catch (MessageQueueException mqe)
+			{
+				Console.WriteLine("{0} {1}", mqe.Message, mqe.MessageQueueErrorCode);
+			}
+
+        	
+        	Receive();
         }
+
+		private void Receive()
+		{
+			if (_isRunning)
+			{
+				_channel.BeginReceive(new TimeSpan(0, 0, 0, ConfigurationSettings.PollingTimeout));
+			}
+		}
     }
 }
--- EventDrivenConsumer/EventDrivenConsumer/Sender/Sender/Program.cs
+++ EventDrivenConsumer/EventDrivenConsumer/Sender/Sender/Program.cs
@@ -5,27 +5,33 @@ namespace Sender
 {
     class Program
     {
-        //sender -m="my name is" -c=polling_consumer
+		private static string _channel;
+		private static string _message;
+
         static void Main(string[] args)
         {
-            var channel = string.Empty;
-            var message = string.Empty;
-            var p = new OptionSet()
-                        {
-                            {"c|channel=", v => channel = v},
-                            {"m|message=", m => message = m}
-                        };
-            p.Parse(args);
+            _channel = "bob3";
+            _message = "This is also a message";
+			//var p = new OptionSet()
+			//            {
+			//                {"c|channel=", v => channel = v},
+			//                {"m|message=", m => message = m}
+			//            };
+			//p.Parse(args);
             
-            if (CheckArguments(message, channel))
-            {
-                return;
-            }
+			//if (CheckArguments(message, channel))
+			//{
+			//    return;
+			//}
 
-            string channelName = string.Format(@".\private$\{0}", channel);
+            string channelName = string.Format(@".\private$\{0}", _channel);
 
             var producer = new Producer(channelName);
-            producer.Send(message);
+        	for (int i = 0; i < 5; i++)
+        	{
+				producer.Send(_message);
+        	}
+        	Console.ReadKey();
         }
         
         private static bool CheckArguments(string message, string channel)
